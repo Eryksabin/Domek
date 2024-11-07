90b7fa6ff2681eedcaac4954c34b981c39970b7b
Zoraris <eryk.sabiniewicz@gmail.com>
2024-11-07 11:23:50 +0100

    Done

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,45 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button likebutton;
+    private Button deleteButton;
+    private TextView likesView;
+    private int countLikes = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        likebutton = findViewById(R.id.likeButton);
+        deleteButton = findViewById(R.id.deleteButton);
+        likesView = findViewById(R.id.likesView);
+
+        likebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countLikes++;
+                updateCountLikes();
+            }
         });
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(countLikes > 0) {
+                    countLikes--;
+                }
+                updateCountLikes();
+            }
+        });
+    }
+    public void updateCountLikes() {
+        likesView.setText(countLikes + " polubień");
     }
 }
